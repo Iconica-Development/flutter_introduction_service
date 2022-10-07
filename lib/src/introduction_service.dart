9f6cba3f09a14dc3ff2f3697a441b4e26462b454
Niels Gorter <gorterniels@linuxmail.org>
2022-10-07 16:13:16 +0200

    changed service

--- lib/src/introduction_service.dart
+++ lib/src/introduction_service.dart
@@ -1,12 +1,10 @@
 import 'package:flutter_introduction_interface/flutter_introduction_interface.dart';
 
 class IntroductionService {
-  IntroductionService([this.dataProvider]) {
-    _dataProvider = dataProvider ?? LocalIntroductionDataProvider();
-  }
+  IntroductionService([IntroductionInterface? dataProvider])
+      : _dataProvider = dataProvider ?? LocalIntroductionDataProvider();
 
-  final IntroductionInterface? dataProvider;
-  late IntroductionInterface _dataProvider;
+  late final IntroductionInterface _dataProvider;
 
   Future<void> onSkip() {
     return _dataProvider.setCompleted(true);
